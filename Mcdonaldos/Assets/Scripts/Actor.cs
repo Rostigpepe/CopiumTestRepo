6d20e39410fff0be7aa17be5b8d908bedc44438e
jakob123100 <bokajnevs1@gmail.com>
2023-02-02 00:04:22 +0100

    actor script

---
+++ Mcdonaldos/Assets/Scripts/Actor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+
+    private int health;
+
+    private bool IsDead()
+    {
+        return health >= 0;
+    }
+
+	private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+	public void TakeDamange(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (IsDead())
+        {
+            Die();
+        }
+    }
+}
